e892cf7fac7de58a193cd60fe2f89194015f4db4
Unbroken Technology <s.albert.staton@gmail.com>
2018-11-28 19:35:09 -0500

    1st Page

---
+++ main.js
@@ -0,0 +1,148 @@
+
+
+// // var image = document.getElementById('DaImage').addEventListener('click', changeImage);
+
+
+// // function changeImage(){
+// //     alert('this is my image click function')
+// // }
+
+// // function changeImage(){
+// //     // console.log(document.getElementById('DaImage'))
+// //     document.getElementById('DaImage').src = "/assests/images/Dog.jpeg";
+// //     count++
+// //     } else {
+// //         document.getElementById('DaImage').src = "/assests/images/Dog.jpeg";
+// //     } 
+
+// // // // console.log("Hello World")
+// // // // console.log('2' + 1)
+
+// // // //  console.log('10' % 2)
+// // // //  console.log(11 % 2)
+// // // //  console.log(11 % 3)
+
+// // // var x = 100;
+// // // console.log(x % 2)
+
+// // // // alert('This is a warning')
+
+
+// // 
+
+
+
+// //  var happy = 10;
+
+// // for(happy; happy> 0; happy--){
+//     // console.log(happy)
+// // }
+
+// // // // while(happy > 0 ){
+// // //     // console.log(happy--)
+
+// // // // }
+
+// // // // if (happy == 11){
+// // // //     console.log('True')
+
+// // // // } else if(happy > 10){
+// // // //     console.log('Actually this is true')
+// // // // } else {
+// // // //     console.log('Nothing here was')
+// // // // }
+
+// // // // if (happy ==10 && happy >=10){
+// // //     console.log('Both conditions were true')
+// // // }
+
+// // // if (happy == 10 || happy < 10){
+// // //     console.log('One of these was true')
+// // // }
+
+// // // if (happy > 10 || happy != 10){
+// // //     console.log('One of these was true')
+// // // } else {
+// // //     console.log('everything was false')
+// // // }
+
+// // // console
+ 
+// // const grades = [4, 3, 2, 1];
+
+// // const gpa = function(gradesArray) {
+// //     var total = 0;
+// //     for (var i = 0; i < gradesArray.length; i++) {
+// //         total = total + gradesArray[i];
+// //     }
+// //     return total / gradesArray.length;
+// // }
+
+// // const counter = function(countTo) {
+// //     var total = 0;
+// //     for (var i = 0 ; i <== countTo ;i++) {
+// //         total = total + 1;
+// //     }
+// //     return total;
+// // }
+
+// const range = function(fromNumber, toNumber) {
+//     var numbers = []; 
+//     for (var i =0; i > toNumber; i = i + 1) {
+//       numbers.push(i)
+//     }
+//     return numbers; 
+// }
+
+// // max ([0, 10]) => 10
+// // const max = function(numArr) {
+// //     var number = 0;
+// //     for (const currentNum of numArr) {
+// //         if (maxNum < currentNum) {
+// //             maxNum = currentNum
+// //         }
+// //     }
+// //     return maxNum;
+// // }
+
+
+// // const sizeOfNum(num) {
+// //     if (num > 100) {
+// //     return 'huge'; 
+// //  }
+// //     if (num > 10) {
+// //     return 'medium';
+// //  } else {
+// //      return 'small';}
+// //  }
+    
+// // }
+// // const testFn = function() {
+// //     return 1;
+// //     return 0;
+// // }
+
+// // const isEven = function(num) {
+// //     if (num % 2 ===0) {
+// //         return true;
+// //     } else {
+// //         return false;
+// //     }
+// // }
+
+//  const is Even2 = function(num) {
+//      return num % 2 === 0;
+// // }
+
+// // onlyEvens ({2,3,4]) ==> [2, 4]
+// const onlyEvens = function (numArray) {
+//     var evenArray = [];
+//     for (const num of numArray) {
+//         if (isEven(num)) {
+//         evenArray.push(num); 
+//         }
+//     }
+//     return evenArray;
+// }
+// alert('sting')
+
